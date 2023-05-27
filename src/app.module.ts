d7c1cd909ece1a07f82cdee97a864f81ed7734d5
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-27 23:26:22 +0300

    подключение через ormmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'student',
+    password: 'student',
+    database: 'kupipodariday',
+    entities: [],
+    synchronize: true,
+  })],
 })
 export class AppModule {}
