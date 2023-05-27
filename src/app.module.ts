8b7f4a9f8cf05717fc82004e7d5f1fda6b8df541
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-27 23:26:41 +0300

    orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'student',
-    password: 'student',
-    database: 'kupipodariday',
-    entities: [],
-    synchronize: true,
-  })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
