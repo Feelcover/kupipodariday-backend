c79a95cb5854255dfc6097812a8556740ebfddb4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-28 15:17:59 +0300

    фикс

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(helmet());
   await app.listen(3000);
 }
