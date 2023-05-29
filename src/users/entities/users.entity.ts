f3354628ffbab25db1c973d9478d453b3ae43004
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-29 16:16:25 +0300

    часть сущности для users

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,5 +1,24 @@
-import { Entity } from 'typeorm';
+import { IsNotEmpty, Length } from 'class-validator';
+import { Column, Entity } from 'typeorm';
 import { MainEntity } from '../../utils/MainEntity.entity';
 
 @Entity()
-export class User extends MainEntity {}
+export class User extends MainEntity {
+    @Column({unique: true})
+    @IsNotEmpty()
+    @Length(2, 30)
+    username: string;
+
+    @Column({default:"Пока ничего не рассказал о себе"})
+    @Length(2, 200)
+    about: string;
+
+    @Column({default:"https://i.pravatar.cc/300"})
+    avatar: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+}
