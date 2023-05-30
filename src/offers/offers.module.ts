7bb249c3a23eaf6ff853f24fcd147d84cecfd7f0
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-05-30 17:42:29 +0300

    заготовка для модуля offers

---
+++ src/offers/offers.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Offer } from './entities/offer.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Offer])],
+  exports: [],
+  controllers: [],
+  providers: [],
+})
+export class OfferModule {}
