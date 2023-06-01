73ab51d916f3d4b970fa023348e80d719b9f9ed4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-01 11:33:54 +0300

    users правка логики

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsUrl, Length } from 'class-validator';
 import { Offer } from 'src/offers/entities/offer.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
 import { WishList } from 'src/wishlists/entities/wishlist.entity';
@@ -14,13 +14,16 @@ export class User extends MainEntity {
 
   @Column({ default: 'Пока ничего не рассказал о себе' })
   @Length(2, 200)
+  @IsOptional()
   about: string;
 
   @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsOptional()
   @IsUrl()
   avatar: string;
 
   @Column({ unique: true })
+  @IsEmail()
   email: string;
 
   @Column({ select: false })
@@ -29,7 +32,7 @@ export class User extends MainEntity {
   @OneToMany(() => Wish, (wish) => wish.owner)
   wishes: Wish[];
 
-  @OneToMany(() => WishList, (wishlist) => wishlist.items)
+  @OneToMany(() => WishList, (wishlist) => wishlist.owner)
   wishlists: WishList[];
 
   @OneToMany(() => Offer, (offer) => offer.user)
