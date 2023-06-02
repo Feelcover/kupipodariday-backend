7d2d05826ee40b83266e46ebdc65e6e4e4abf496
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-02 12:58:14 +0300

    фикс

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -10,26 +10,31 @@ import {
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
   @Length(2, 30, {
     message: 'Имя должно быть от 2 до 30 символов',
   })
   username: string;
 
   @IsOptional()
+  @IsString()
   @Length(2, 200, {
     message: 'Описание должно быть от 2 до 200 символов',
   })
   about: string;
 
   @IsOptional()
+  @IsString()
   @IsUrl({}, { message: 'Введите корректную ссылку' })
   avatar: string;
 
   @IsNotEmpty()
+  @IsString()
   @IsEmail({}, { message: 'Введите корректный Email' })
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   @MinLength(4, { message: 'Пароль должен быть длинной минимум 4 символа' })
   password: string;
 }
