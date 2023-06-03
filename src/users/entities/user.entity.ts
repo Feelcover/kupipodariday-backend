cbe9678706dec9d27d5fdde0b120227109c2ec09
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-03 13:03:27 +0300

    корректировки

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsUrl, Length } from 'class-validator';
+import { Exclude } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl, Length } from 'class-validator';
 import { Offer } from 'src/offers/entities/offer.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
 import { WishList } from 'src/wishlists/entities/wishlist.entity';
@@ -10,23 +11,29 @@ export class User extends MainEntity {
   @Column({ unique: true })
   @IsNotEmpty()
   @Length(2, 30)
+  @IsString()
   username: string;
 
   @Column({ default: 'Пока ничего не рассказал о себе' })
   @Length(2, 200)
   @IsOptional()
+  @IsString()
   about: string;
 
   @Column({ default: 'https://i.pravatar.cc/300' })
   @IsOptional()
   @IsUrl()
+  @IsString()
   avatar: string;
 
   @Column({ unique: true })
   @IsEmail()
+  @IsString()
   email: string;
 
   @Column({ select: false })
+  @IsString()
+  @Exclude()
   password: string;
 
   @OneToMany(() => Wish, (wish) => wish.owner)
