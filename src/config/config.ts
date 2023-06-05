fd47907f1e0ba9cf8be8df20b879009de43c797c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-05 13:45:01 +0300

    prettier

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,10 @@
 export default () => ({
-    database: {
-        host: 'localhost',
-        port: 5432,
-        username: 'student',
-        password: 'student',
-        database: 'kupipodariday',
-    },
-    secretKey: process.env.JWT_SECRET || 'jwt-secret',
+  database: {
+    host: 'localhost',
+    port: 5432,
+    username: 'student',
+    password: 'student',
+    database: 'kupipodariday',
+  },
+  secretKey: process.env.JWT_SECRET || 'jwt-secret',
 });
