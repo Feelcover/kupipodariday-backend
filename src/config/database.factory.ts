557e50f31756b5d2d28f7743fbce0a718ec8e5ba
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-05 13:54:27 +0300

    TypeOrmOptionsFactory

--- src/config/database.factory.ts
+++ src/config/database.factory.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { ConfigService } from '@nestjs/config';
 
 
 @Injectable()
 export class DatabaseFactory implements TypeOrmOptionsFactory {
-    
-}
+    constructor(private configService: ConfigService) {
+
+    }
