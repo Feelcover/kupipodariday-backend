6e20a3c5c340406d9bb6794df00c0aca1a44ef58
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-06 14:12:24 +0300

    app модуль

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './config/config';
+import { DatabaseFactory } from './config/database-config.factory';
 import { OffersModule } from './offers/offers.module';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
@@ -9,10 +11,12 @@ import { WishlistModule } from './wishlists/wishlists.module';
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal:true, load: [config]}),
+    TypeOrmModule.forRootAsync({useClass: DatabaseFactory}),
     UsersModule,
     WishlistModule,
     WishesModule,
     OffersModule,
   ],
+  providers: [DatabaseFactory],
 })
 export class AppModule {}
