d8482fd0fabc7d9fe0fb6f6c46b51253e1b6686d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-07 14:25:44 +0300

    фикс

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config from './config/config';
+import configuration from './config/configuration';
 import { DatabaseFactory } from './config/database-config.factory';
 import { OffersModule } from './offers/offers.module';
 import { UsersModule } from './users/users.module';
@@ -10,7 +10,7 @@ import { WishlistModule } from './wishlists/wishlists.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal:true, load: [config]}),
+    ConfigModule.forRoot({isGlobal:true, load: [configuration]}),
     TypeOrmModule.forRootAsync({useClass: DatabaseFactory}),
     UsersModule,
     WishlistModule,
