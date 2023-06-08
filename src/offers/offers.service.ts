28cde873bafea39ceebcca086d9c0ef796947c0e
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 11:54:24 +0300

    заготовка wishesservice

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { WishesService } from "src/wishes/wishes.service";
+import { DataSource, Repository } from "typeorm";
+import { Offer } from "./entities/offer.entity";
 
 
 @Injectable()
 export class OffersService {
-    
+    constructor(
+        private dataSource: DataSource,
+        @InjectRepository(Offer)
+        private offerRepository: Repository<Offer>,
+        private wishesService: WishesService,
+        ) {}
 }
