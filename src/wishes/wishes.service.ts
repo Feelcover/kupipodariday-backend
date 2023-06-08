d7f921679f9b26a22b94ad69e3577a379c0c465d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 16:31:55 +0300

    заготовка для методов wishes

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DataSource, Repository } from "typeorm";
+import { Wish } from "./entities/wish.entity";
 
 
 @Injectable()
 export class WishesService {
+constructor(
+    private dataSource: DataSource,
+    @InjectRepository(Wish)
+    private wishesRepository: Repository<Wish>
+) {}
 
 }
