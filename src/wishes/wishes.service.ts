753f4a061c4a85252d984012f330ae0c5b7f5188
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 17:10:05 +0300

    убрал линшнее

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,15 +1,29 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { DataSource, Repository } from "typeorm";
-import { Wish } from "./entities/wish.entity";
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  DataSource,
+  FindManyOptions,
+  FindOneOptions,
+  Repository,
+} from 'typeorm';
+import { CreateWishDto } from './dto/create-wish.dto';
+import { Wish } from './entities/wish.entity';
 
 @Injectable()
 export class WishesService {
-constructor(
+  constructor(
     private dataSource: DataSource,
     @InjectRepository(Wish)
-    private wishesRepository: Repository<Wish>
-) {}
+    private wishesRepository: Repository<Wish>,
+  ) {}
 
+   findAll(query: FindManyOptions<Wish>) {
+    return this.wishesRepository.find(query);
+  }
+
+   findOne(query: FindOneOptions<Wish>) {
+    return this.wishesRepository.findOne(query);
+  }
+
+  async create(createWishDto: CreateWishDto, ownerId: number) {}
 }
