9a083e213b091f29f8fd2805795edb4ae5cd72e4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 12:08:50 +0300

    метор create для offers.service

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,16 +1,53 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { WishesService } from "src/wishes/wishes.service";
-import { DataSource, Repository } from "typeorm";
-import { Offer } from "./entities/offer.entity";
-
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { WishesService } from 'src/wishes/wishes.service';
+import { DataSource, Repository } from 'typeorm';
+import { CreateOfferDto } from './dto/create-offer.dto';
+import { Offer } from './entities/offer.entity';
 
 @Injectable()
 export class OffersService {
-    constructor(
-        private dataSource: DataSource,
-        @InjectRepository(Offer)
-        private offerRepository: Repository<Offer>,
-        private wishesService: WishesService,
-        ) {}
+  constructor(
+    private dataSource: DataSource,
+    @InjectRepository(Offer)
+    private offerRepository: Repository<Offer>,
+    private wishesService: WishesService,
+  ) {}
+
+  async create(createOfferDto: CreateOfferDto, userId: number) {
+    const { amount, itemId } = createOfferDto;
+    const wish = await this.wishesService.findOne({
+      where: { id: itemId },
+      relations: ['owner'],
+    });
+    const { price, raised, owner } = wish;
+
+    if (owner.id === userId) {
+      throw new ForbiddenException(
+        'Вы не можете вносить деньги на свои подарки',
+      );
+    }
+
+    if (amount + raised > price) {
+      throw new ForbiddenException(
+        `Сумма взноса превышает сумму остатка стоимости подарка на ${
+          amount + raised - price
+        } руб.`,
+      );
+    }
+    const offer = this.offerRepository.create({
+      ...createOfferDto,
+      user: { id: userId },
+      item: { id: itemId },
+    });
+
+    await this.dataSource.transaction( async(transManager) => {
+        await transManager.insert<Offer>(Offer, offer);
+        await transManager.update<Wish>(Wish, itemId, {
+            raised: amount + raised,
+        });
+    })
+    return {}
+  }
 }
