a2ec0a9fca60998508a559d8a344aaac570c9876
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 11:42:41 +0300

    заготовка стратегии local

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-jwt";
 
-
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportStrategy(Strategy){
+    
 }
