2b7a50743d6f6db392be19df58543347f8b33727
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-08 12:27:11 +0300

    подключение в модуле offers

--- src/offers/offers.module.ts
+++ src/offers/offers.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from 'src/users/users.module';
 import { WishesModule } from 'src/wishes/wishes.module';
 import { Offer } from './entities/offer.entity';
+import { OffersController } from './offers.controller';
+import { OffersService } from './offers.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Offer]), WishesModule],
-  exports: [],
-  controllers: [],
-  providers: [],
+  controllers: [OffersController],
+  providers: [OffersService],
 })
 export class OffersModule {}
