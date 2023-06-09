006eae68a97425406a76bd041db5c36be1ebae37
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-09 11:39:25 +0300

    несколько методов контроллера

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, UseGuards } from "@nestjs/common";
-import { JwtGuard } from "src/auth/guards/jwt.guard";
-import { WishesService } from "./wishes.service";
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { WishesService } from './wishes.service';
 
-@Controller()
+@Controller('wishes')
 export class WishesController {
-    constructor (private readonly wihesService: WishesService){}
-    @UseGuards(JwtGuard)
+  constructor(private readonly wishesService: WishesService) {}
 
+  @Get('last')
+  getLastWishes() {
+    return this.wishesService.getLast();
+  }
 
+  @Get('top')
+  getTopWishes() {
+    return this.wishesService.getTop();
+  }
+
+@UseGuards(JwtGuard)
+@Get(':id')
+getById(@Param('id') id: string) {
+return this.wishesService.getById(+id)
+}
 }
