65317624b9abbb423e592a767468af4fa449d45d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-09 12:11:18 +0300

    create метод

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HashService } from 'src/hash/hash.service';
 import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -18,5 +19,33 @@ export class UsersService {
 
   findOne(query: FindOneOptions<User>) {
     return this.userRepository.findOne(query);
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, username, password } = createUserDto;
+    const userEmail = await this.findOne({
+      where: [{ email }],
+    });
+    const userName = await this.findOne({
+      where: [{ username }],
+    });
+    if (userEmail) {
+      throw new ConflictException(
+        'Пользователь с таким email уже зарегистрирован',
+      );
+    }
+    if (userName) {
+      throw new ConflictException(
+        'Пользователь с таким именем пользовтеля уже зарегистрирован',
+      );
+    }
+
+    const hash = await this.hashService.generate(password);
+    const newUser = this.userRepository.create({
+      ...createUserDto,
+      password: hash,
+    });
+
+    return this.userRepository.save(newUser);
   }
 }
