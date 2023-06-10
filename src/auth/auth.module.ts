04db09d1663b950541f820ebcf5a45b41b4c10a3
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-10 23:11:31 +0300

    заполнил импорты в модуле auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtConfig } from "src/config/jwt-config.factory";
+import { HashModule } from "src/hash/hash.module";
+import { UsersModule } from "src/users/users.module";
 
 
 @Module({
-    imports:[],
+    imports:[
+        PassportModule,
+        UsersModule,
+        HashModule,
+        JwtModule.registerAsync({
+            useClass: JwtConfig
+        })
+    ],
     providers:[],
     controllers:[],
     exports:[],
