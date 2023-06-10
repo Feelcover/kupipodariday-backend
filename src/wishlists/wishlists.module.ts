6811ee46da2cc7ee3c21a0138b19ea9f0cfcc701
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-10 16:42:31 +0300

    подключение в модуле wishlists

--- src/wishlists/wishlists.module.ts
+++ src/wishlists/wishlists.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WishesModule } from 'src/wishes/wishes.module';
 import { WishList } from './entities/wishlist.entity';
+import { WishlistsController } from './wishlists.controller';
+import { WishlistService } from './wishlists.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WishList]), WishesModule],
-  exports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([WishList])],
+  controllers: [WishlistsController],
+  providers: [WishlistService],
+  exports: [WishlistService],
 })
 export class WishlistModule {}
