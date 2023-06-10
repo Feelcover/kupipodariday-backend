6e779109a3985120202ceb4cb43d012cedcadb97
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-10 23:07:28 +0300

    часть стратегии jwt

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "src/users/users.service";
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(configService: ConfigService, private userService: UsersService){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get('secretKey'),
+        })
+    }
 }
