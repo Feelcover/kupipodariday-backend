93403ba52435869f397e3243e56f5aba4f5e8ac6
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-11 22:57:22 +0300

    signin запрос

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Controller } from "@nestjs/common";
-import { UsersService } from "src/users/users.service";
-import { AuthService } from "./auth.service";
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { CustomRequest } from 'src/utils/CustomRequest';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
-    constructor(
-        private authService: AuthService,
-        private userService: UsersService,
-    ){}
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  signin(@Req() req: CustomRequest) {
+    return this.authService.auth(req.user);
+  }
 }
