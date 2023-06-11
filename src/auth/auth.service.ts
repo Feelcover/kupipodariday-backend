c38c215816eef015cfbec0094f23f600f9342894
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-11 22:43:57 +0300

    конструктор для authservice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { HashService } from "src/hash/hash.service";
+import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class AuthService {
-    
+    constructor(
+        private jwtService: JwtService,
+        private userService: UsersService,
+        private hashService: HashService,
+      ) {}
 }
