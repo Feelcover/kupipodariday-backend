ac40dd01ac843da51ff120cc6f2f918f9714c027
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-06-11 22:45:32 +0300

    функция auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { HashService } from "src/hash/hash.service";
+import { User } from "src/users/entities/user.entity";
 import { UsersService } from "src/users/users.service";
 
 @Injectable()
@@ -10,4 +11,12 @@ export class AuthService {
         private userService: UsersService,
         private hashService: HashService,
       ) {}
+
+      auth(user:User ) {
+        const payload = {sub: user.id};
+
+        return {
+            access_token: this.jwtService.sign(payload, {expiresIn: '24h'}),
+        }
+      }
 }
